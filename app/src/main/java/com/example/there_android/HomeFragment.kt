a138c03d5e28a2c100857646d1855a8d2800ff19
jelee2555 <jieun4510@sju.ac.kr>
2022-07-20 01:51:45 +0900

    FEAT_bnv

---
+++ app/src/main/java/com/example/there_android/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.there_android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.there_android.databinding.FragmentHomeBinding
+
+class HomeFragment: Fragment() {
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/there_android/MainActivity.kt
+++ app/src/main/java/com/example/there_android/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.there_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.there_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initBottomNavigation(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+
+        binding.mainBnv.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.searchFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, SearchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.chattingFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, ChattingFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.mypageFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, MyPageFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.etcFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, EtcFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
