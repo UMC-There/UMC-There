c8fee278078838652ea632103fea974d0c3ccbf4
chaeli <s.kcl5243@gmail.com>
2022-08-04 17:59:54 +0900

    CHORE: name

--- app/src/main/java/com/example/there_android/AddPostResponse.kt
+++ app/src/main/java/com/example/there_android/AddPostResponse.kt
@@ -2,9 +2,13 @@ package com.example.there_android
 
 import com.google.gson.annotations.SerializedName
 
-data class AuthResponse(
+data class AddPostResponse(
     @SerializedName("code") val code: Int,
     @SerializedName("isSuccess") val isSuccess: Boolean,
     @SerializedName("message") val message: String,
-    @SerializedName("result") val result:
-)
+    @SerializedName("result") val result: Result? = null
+){
+    data class Result(
+        @SerializedName("postIdx") var postIdx : Int
+    )
+}
