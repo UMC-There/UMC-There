0e90aed68e4547bb813e2192055d2415d759adc1
jelee2555 <jieun4510@sju.ac.kr>
2022-08-14 02:42:14 +0900

    FIX_Chatlist_Server

--- app/src/main/java/com/example/there_android/ChatData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.there_android
-
-data class ChatData (
-     val nickname : String,
-     val time : Int,
-     val text : String,
-     val count : Int,
-     val profile : String
-)
